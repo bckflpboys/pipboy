a169bbb374b93dd7cd1fe79900a70e9d36b42df9
bckflpboys <crushadde@gmail.com>
2025-04-29 10:33:17 +0200

    routes fix

--- app/api/user/profile/route.ts
+++ app/api/user/profile/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
 import { Session } from 'next-auth';
-import { authOptions } from '../../auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth';
 import clientPromise from '@/lib/mongodb';
 import { User, USERS_COLLECTION } from '../../../models/User';
 
